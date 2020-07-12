38ce6c158b522397f807ca5dbd47617752667321
affu999 <50880987+affu999@users.noreply.github.com>
2020-07-12 19:37:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <Navigation />
       <Router>
         <Route exact path={"/"} component={BlogPage} />
-        <Route exact path={"https://techfierceblogs.netlify.app/blog-content/:id"} component={BlogContent} />
+        <Route exact path={"/blog-content/:id"} component={BlogContent} />
       </Router>
     </>
   );
