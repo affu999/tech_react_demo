4c7fa0ec025a2437552438cfde7a7b01f7d92fbc
affu999 <50880987+affu999@users.noreply.github.com>
2020-07-14 14:43:43 +0530

    Add files via upload

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../CSS/Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="container-fluid footer pb-3 mb-3">
+        <h3 className="text-center pt-2">Tech Fierce Blog Page</h3>
+        <h6 className="text-center">
+          &#169; Copyright 2020 Tech Fierce All rights reserved
+        </h6>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
