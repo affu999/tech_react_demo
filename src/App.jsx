6301080d7514a50aa4e312d2afd5c01494929d61
affu999 <50880987+affu999@users.noreply.github.com>
2020-07-14 14:48:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./Components/Navigation";
+import Footer from "./Components/Footer";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "./CSS/Navbar.css";
@@ -16,6 +17,7 @@ const App = () => {
         <Route exact path={"/"} component={BlogPage} />
         <Route exact path={"/blog-content/:id"} component={BlogContent} />
       </Router>
+      <Footer />
     </>
   );
 };
