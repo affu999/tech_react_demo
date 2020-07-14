0aacc28d6432f475c6bc837a19c195de0997f81d
affu999 <50880987+affu999@users.noreply.github.com>
2020-07-14 16:17:22 +0530

    Update Navigation.jsx

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -9,7 +9,7 @@ const Navigation = () => {
             <nav className="navbar navbar-expand-lg navbar-light">
               <div className="container-fluid ml-4">
                 <img
-                  src="https://images.unsplash.com/photo-1534723328310-e82dad3ee43f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                  src="https://lh3.googleusercontent.com/iDFGIhFMUhMiMGGyx-cijYlv4nYZY3_JGNOwTtGJh0etnKP1jwoqtU7H_dBsQEmS9wLHUAjzJ0amQycC9UKJr4IeFY5Mc9srPneJ_CmXiL2zXY5d4yZIPEQzatLfigQPhHDwoo4=w1920-h1080"
                   alt="..."
                 />
                 <h1 className="companyName">Tech Fierce</h1>
