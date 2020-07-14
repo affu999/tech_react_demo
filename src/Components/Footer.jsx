fc2a85f5460e9af327a5482125de49f0b331d583
affu999 <50880987+affu999@users.noreply.github.com>
2020-07-14 14:58:40 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="container-fluid footer pb-3 mb-3">
         <h3 className="text-center pt-2">Tech Fierce Blog Page</h3>
         <h6 className="text-center">
-          &#169; Copyright 2020 Tech Fierce All rights reserved
+          &#169; Copyright 2020 Tech Fierce. All rights reserved
         </h6>
         <h6 className="text-center">
           Designed and developed by <strong>Affan Sharief</strong>
